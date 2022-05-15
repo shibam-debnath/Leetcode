8be87d0a9093bd063c08a0194253931b94e89a4a
noob_coder <shibamdebnath07@gmail.com>
2022-05-15 12:28:12 +0530

    q1

---
+++ 1easy/1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+
+        solve();
+    }
+    return 0;
+}
