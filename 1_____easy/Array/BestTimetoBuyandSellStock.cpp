34216fdf69bae816b0abcbfee253adedde8e40c6
noob_coder <shibamdebnath07@gmail.com>
2022-05-16 20:39:52 +0530

    Runtime:faster than 78.52% & Memory Usage: less than 78.52%

---
+++ 1_____easy/Array/BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,57 @@
+// Shibam Debnath
+// May 16,2022
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int maxProfit(vector<int> &p)
+{
+    int n = p.size();
+    int dp[n + 1][n + 1];
+    int min = ;
+    int sum = 0;
+    for (int j = 2; j <= n; j++)
+    {
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 2; j <= n; j++)
+        {
+            if ()
+        }
+    }
+
+    // int profit = 0;
+    // int l = 0;
+    // int r = p.size();
+    // int val = 0;
+    // while (l<r)
+    // {
+    //     val = p[l] - p[r];
+    //     if(val > profit){
+    //         profit = val;
+    //     }
+    //     if(p[l+1] < p[l]){
+    //         l++;
+    //         continue;
+    //     }
+    //     if(p[r-1] > p[r]){
+    //         r--;
+    //         continue;
+    //     }
+
+    //     l++;
+    //     r--;
+
+    // }
+
+    // return profit;
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
