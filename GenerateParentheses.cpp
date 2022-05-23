be9c5d0a9f84b1ee7ffbb7ffcd31052f0e284490
noob_coder <shibamdebnath07@gmail.com>
2022-05-23 23:47:31 +0530

    Medium_Amazon

---
+++ GenerateParentheses.cpp
@@ -0,0 +1,42 @@
+// Shibam Debnath
+// May 23,2022
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// Runtime: 3 ms, faster than 84.10% of C++ online submissions for Generate Parentheses.
+// Memory Usage: 15.5 MB, less than 24.46% of C++ online submissions for Generate Parentheses
+
+
+void generate(string s, int l, int r, vector<string> &ans)
+{
+    if(l==0 && r==0){
+        ans.push_back(s);
+        return;
+    }
+
+    if(l>0){
+        s.push_back('(');
+        generate(s, l-1, r, ans);
+    }
+
+    if (r>0 && r>l)
+    {
+        s.push_back(')');
+        generate(s, l, r-1, ans);
+    }
+    
+}
+
+vector<string> generateParenthesis(int n)
+{
+    vector<string> ans;
+    string s="";
+    generate(s, 0, 0, ans);
+}
+int main()
+{
+    // Main function goes here
+    return 0;
+}
