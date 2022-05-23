4977bb5989acb59eb86d948b36b688b2e5aa4385
noob_coder <shibamdebnath07@gmail.com>
2022-05-23 23:48:01 +0530

    Medium_Amazon

--- GenerateParentheses.cpp
+++ GenerateParentheses.cpp
@@ -8,25 +8,27 @@ using namespace std;
 // Runtime: 3 ms, faster than 84.10% of C++ online submissions for Generate Parentheses.
 // Memory Usage: 15.5 MB, less than 24.46% of C++ online submissions for Generate Parentheses
 
-
 void generate(string s, int l, int r, vector<string> &ans)
 {
-    if(l==0 && r==0){
+
+    if (l == 0 && r == 0)
+    {
         ans.push_back(s);
         return;
     }
 
-    if(l>0){
+    if (l > 0)
+    {
         s.push_back('(');
-        generate(s, l-1, r, ans);
+        generate(s, l - 1, r, ans);
+        // s.pop_back();
     }
 
-    if (r>0 && r>l)
+    if (r > 0 && r > l)
     {
         s.push_back(')');
-        generate(s, l, r-1, ans);
+        generate(s, l, r - 1, ans);
     }
-    
 }
 
 vector<string> generateParenthesis(int n)
