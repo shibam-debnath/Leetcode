c72a0a7070cae9dc3ab02cc9e322b0f61abaab02
noob_coder <shibamdebnath07@gmail.com>
2022-05-28 11:27:17 +0530

    Easy_Leetcode

---
+++ LeetCodeDaily/middle-of-the-linked-list.cpp
@@ -0,0 +1,31 @@
+// Shibam Debnath
+// May 26,2022
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+struct ListNode
+{
+    int val;
+    ListNode *next;
+                                                                                                                                      *
+};
+
+ListNode *middleNode(ListNode *head)
+{
+    ListNode *left = head;
+    ListNode *right = head;
+
+    while (right && right->next)
+    {
+        left = left->next;
+        right = right->next->next;
+    }
+
+    return left;
+}
+int main()
+{
+    // Main function goes here
+    return 0;
+}
