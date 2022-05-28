b2c4b1a65b58bec59168dbc433df3dbbd228b02e
noob_coder <shibamdebnath07@gmail.com>
2022-05-28 13:06:56 +0530

    Medium_LinkedList

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -24,6 +24,50 @@ bool isPalindrome(ListNode *head)
 
     return s == t;
 }
+
+// 0(1) space
+bool isPalindrome(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    ListNode *rev;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    rev = slow;
+    slow = slow->next;
+    rev->next = NULL;
+
+    ListNode *temp = head;
+    while (slow)
+    {
+        temp = slow->next;
+        slow->next = rev;
+        rev = slow;
+        slow = temp;
+    }
+    // now int the first half we have normal form but from middle half we have reverse form
+    // 1->2->3<-2 < -1
+
+    slow = head;
+    fast = rev;
+
+    while (fast)
+    {
+        if (fast->val != slow->val)
+            return false;
+        else
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     // Main function goes here
