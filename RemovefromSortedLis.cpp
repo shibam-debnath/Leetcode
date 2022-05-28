312775fd02fb8fd6709fe2b6443cfdafdea86396
noob_coder <shibamdebnath07@gmail.com>
2022-05-28 11:58:46 +0530

    Easy_LinkedList

---
+++ RemovefromSortedLis.cpp
@@ -0,0 +1,38 @@
+// Shibam Debnath
+// May 26,2022
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+struct ListNode
+{
+    int val;
+    ListNode *next;
+};
+ListNode *deleteDuplicates(ListNode *head)
+{
+    if (!head)
+    {
+        return head;
+    }
+
+    ListNode *temp = head;
+    while (temp && temp->next)
+    {
+        if (temp->val == temp->next->val)
+        {
+            temp->next = temp->next->next;
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
+
+    return head;
+}
+int main()
+{
+    // Main function goes here
+    return 0;
+}
