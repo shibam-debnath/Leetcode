4e54ed5516b7955400075f25cc719ed320165ca1
noob_coder <shibamdebnath07@gmail.com>
2022-05-28 12:11:37 +0530

    Easy_LinkedList

--- sort-a-linked-list-of-0s-1s-or-2s.cpp
+++ sort-a-linked-list-of-0s-1s-or-2s.cpp
@@ -1,6 +1,6 @@
-//Shibam Debnath
-//May 26,2022
- 
+// Shibam Debnath
+// May 26,2022
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -10,30 +10,33 @@ struct ListNode
     ListNode *next;
 };
 
-
-void *sort_linked(ListNode *head){
+void *sort_linked(ListNode *head)
+{
     vector<int> v(3, 0);
     ListNode *ptr = head;
-    while(ptr){
+    while (ptr)
+    {
         v[ptr->val] += 1;
         ptr = ptr->next;
     }
     int i = 0;
-    ptr = head;   // again using same ptr 
+    ptr = head; // again using same ptr
     while (ptr)
     {
-        if(v[i] != 0){ //untill no of 0,1,2 is empty keep inserting i
+        if (v[i] != 0)
+        { // untill no of 0,1,2 is empty keep inserting i
             ptr->val = i;
             ptr = ptr->next;
             v[i]--;
         }
-        else{
+        else
+        {
             i++;
         }
     }
-    
 }
-int main() {
-    //Main function goes here
+int main()
+{
+    // Main function goes here
     return 0;
 }
