d919405f97d958912a2b161cf1e5fa9256d3d345
noob_coder <shibamdebnath07@gmail.com>
2022-06-11 12:10:06 +0530

    Medium_Graph

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -23,7 +23,7 @@ int orangesRotting(vector<vector<int>> &grid)
             // starting positions (rotten oranges)
             if (grid[r][c] == 2)
             {
-                q.push({r,c});
+                q.push({r, c});
             }
             else if (grid[r][c] == 1)
             {
@@ -62,6 +62,60 @@ int orangesRotting(vector<vector<int>> &grid)
 
     return ans;
 }
+
+// shortest path 8 directions
+
+class Solution
+{
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        queue<pair<int, int>> pq;
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+        {
+            return -1;
+        }
+
+        pq.push(make_pair(0, 0));
+        // all possible dirs are included here
+        int dx[8] = {1, 0, -1, 0, -1, 1, -1, 1};
+        int dy[8] = {0, 1, 0, -1, -1, -1, 1, 1};
+
+        int newcol = 0;
+        int newrow = 0;
+
+        grid[0][0] = 1;
+
+        while (!pq.empty())
+        {
+            int row = pq.front().first;
+            int col = pq.front().second;
+
+            if (row == n - 1 && col == n - 1)
+            {
+                return grid[row][col];
+            }
+
+            // iterate for all 8 dirs
+            for (int i = 0; i < 8; i++)
+            {
+                newrow = row + dx[i];
+                newcol = col + dy[i];
+
+                if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < n && grid[newrow][newcol] == 0)
+                {
+                    pq.push(make_pair(newrow, newcol));
+                    grid[newrow][newcol] = grid[row][col] + 1;
+                }
+            }
+
+            pq.pop();
+        }
+        return -1;
+    }
+};
 
 int main()
 {
