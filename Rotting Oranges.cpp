b6ff6d73f951cb29b02a2b66be4e4a942660d187
noob_coder <shibamdebnath07@gmail.com>
2022-06-19 22:23:03 +0530

    Medium_Graph

--- Rotting Oranges.cpp
+++ Rotting Oranges.cpp
@@ -69,6 +69,95 @@ public:
     }
 };
 
+class Solution
+{
+public:
+    vector<vector<int>> dir = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+
+    bool NotSafe(int i, int j, int n, int m)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return true;
+
+        return false;
+    }
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+
+        for (int r = 0; r < m; ++r)
+        {
+            for (int c = 0; c < n; ++c)
+            {
+                // starting positions (rotten oranges)
+                if (grid[r][c] == 2)
+                {
+                    q.push({r,
+                            c});
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        while (!q.empty())
+        {
+            int size = q.size();
+            while (size--)
+            {
+                auto x = q.front();
+                q.pop();
+
+                for (size_t i = 0; i < 4; i++)
+                {
+                    int newR = x.first + dir[i][0];
+                    int newC = x.second + dir[i][1];
+
+                    if (newR >= 0 && newR < m && newC >= 0 && newC < n && grid[newR][newC] == 1)
+                    {
+                        grid[newR][newC] = 2;
+                        q.push({newR, newC});
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        if (fresh_exist(grid))
+        {
+            return -1;
+        }
+        else if (minutes == 0)
+        {
+            // no rotten orange present 
+            return 0;
+        }
+        return minutes - 1;
+    }
+
+    bool
+    fresh_exist(vector<vector<int>> &grid)
+    {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < m; c++)
+            {
+                if (grid[r][c] == 1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
 int main()
 {
     // Main function goes here
