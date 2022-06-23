db2dacfbdd37d98449f66b41df180c8d99d234dd
H4saki <shibamdebnath07@gmail.com>
2022-06-23 09:37:47 +0530

    Time: 229 ms (33.08%), Space: 73 MB (43.70%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // check vs code 
+    
     int longestPalindromeSubseq(string s) {
         int n = s.size();
         string t=s;
@@ -8,7 +11,7 @@ public:
         return LIS(dp,t,s);
     }
     
-    int LIS(vector<vector<int>>& dp, string s , string t){
+    int LIS(vector<vector<int>>& dp, string &s , string &t){
         
         int n= s.size();
         
@@ -17,19 +20,10 @@ public:
             for (int j = 1; j <= n; j++)
             {
 
-                if (s[i - 1] == t[j - 1])
-                {
-                    // cout << s[i-1] <<  "  " << i-1 << endl;
-                    // remove 1 from both i.e take both
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else
-                {
-                    // take wo wala jisko leke max val ayega
-                    // take max of both since it is subsequence . if it was substr we should have taken 0
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+                if (s[i - 1] == t[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
                 
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            
             }
         }
         
