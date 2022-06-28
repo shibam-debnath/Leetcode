6e4befb28223b9beafea85f084c4cfbfa2808de0
H4saki <shibamdebnath07@gmail.com>
2022-06-28 14:21:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.94%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long left=0,mid=0,end=x,temp_ans=0;
+        while(end>=left){
+            mid = (left+end)/2;
+            if(mid*mid < x){
+                left = mid+1;
+                // since we may not get accurate , in that case we can return this closest val
+                temp_ans=mid;
+            }
+            else if(mid*mid > x){
+                end = mid-1;
+            }
+            else{
+                // we found
+                return mid;
+            }
+        }
+        
+        return temp_ans; // if we didn't find accurate wala
     }
 };
