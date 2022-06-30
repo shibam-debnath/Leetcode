cb52437a0996ea58773e5cf8eaa61b63ded94fe8
H4saki <shibamdebnath07@gmail.com>
2022-06-30 16:18:31 +0530

    Time: 7 ms (49.01%), Space: 8.9 MB (49.69%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,23 +1,29 @@
 class Solution
 {
-    public:
-        int findPeakElement(vector<int> &nums)
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return 0;
+        // to handle edge cases we are starting from 1 to n-2 since mid+1 & mid-1 may cause errors
+        int left = 1, right = n - 2;
+        while (left <= right)
         {
-            int n = nums.size();
-            if (n == 1) return 0;
-            if (n == 2) return nums[0] > nums[1] ? 0 : 1;
-           	// to handle edge cases we are starting from 1 to n-2
-            int i = 1, j = n - 2;
-            while (i <= j)
-            {
-                int mid = j + (i - j) / 2;
-                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-                else if (nums[mid + 1] > nums[mid]) i = mid + 1;	// go right agar right wala bada dikhe to
-                else if (nums[mid - 1] >= nums[mid]) j = mid - 1;	// go left agar left wala bada dikhe
-            }
-           	// handle edge cases
-            if (nums[0] > nums[1]) return 0;
-            else if (nums[n - 1] > nums[n - 2]) return n - 1;
-            return -1;
+            int mid = right + (left - right) / 2;
+            // peek condition
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if (nums[mid + 1] > nums[mid])
+                left = mid + 1; // go right agar right wala bada dikhe to
+            else if (nums[mid - 1] >= nums[mid])
+                right = mid - 1; // go left agar left wala bada dikhe
         }
+        // handle edge cases
+        if (nums[0] > nums[1])
+            return 0;
+        else if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+        return -1;
+    }
 };
