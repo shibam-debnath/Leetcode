fefd095bc582e0b58c3db6e367071db084e17fc9
H4saki <shibamdebnath07@gmail.com>
2022-07-01 14:13:03 +0530

    Time: 800 ms (5.05%), Space: 74.1 MB (11.70%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *sortList(ListNode *head)
+    {
+        // very important
+        if(!head || !head->next) return head;
+        
+        // take two pointers to divide the linked list into two equal parts
+        ListNode *slow = head, *fast = head;
+        ListNode *middle = NULL;
+
+        while (fast && fast->next)
+        {
+            middle = slow; // since slow ek step aage chala jaata h uuse pehle wala is mid
+            slow = slow->next; // slow is the head of 2nd half
+            fast = fast->next->next;
+        }
+        
+        middle->next = NULL; // end of first half 
+
+        // divide
+        
+        cout << head->val << "  " << slow->val << endl;
+        ListNode *left = sortList(head);
+        
+        ListNode *right = sortList(slow);
+
+        // merge
+        return merge(left, right);
+    }
+
+    ListNode *merge(ListNode *l, ListNode *r)
+    {
+        // create new ListNode and store ans there (initalized with zero)
+        ListNode *node = new ListNode(0);
+
+        // take a pointer to traverse and add
+        ListNode *ptr = node;
+
+        while (l && r)
+        {
+            if (l->val <= r->val)
+            {
+                ptr->next = l;
+                l = l->next;
+            }
+            else
+            {
+                ptr->next = r;
+                r = r->next;
+            }
+            ptr = ptr->next;
+        }
+
+        // if only l exist just add it to the ptr
+        if (l)
+        {
+            ptr->next = l;
+        }
+
+        // if only nright exists
+        if (r)
+        {
+            ptr->next = r;
+        }
+
+        return node->next;
+    }
+};
