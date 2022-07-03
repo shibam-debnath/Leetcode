24147e386fcf5cfa863204de3229df9b27fb398b
H4saki <shibamdebnath07@gmail.com>
2022-07-03 20:10:09 +0530

    Time: 38 ms (5.86%), Space: 17.1 MB (63.54%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
         int gap=0,max_gap=0;
-        int start=0;
+        int start=INT_MIN;
         for(int i=0;i<seats.size();i++){
             if(seats[i]==1){
-                gap=i-start;
-                max_gap = max(max_gap,gap);
+                //left edge case
+                if(start==INT_MIN){
+                    max_gap = i;
+                } 
+                else{
+                    gap=(i-start)/2;
+                    max_gap = max(max_gap,gap);
+                }
                 start=i;
             }
         }
-        // this should be ans with edge cases
-        max_gap/=2;
         
-        // edge from left
-        for(int i=0;i<seats.size();i++){
-            if(seats[i]==1){
-                max_gap=max(max_gap,i);
-                break;
-            }
-        }
-        
-        // edge from right
-        for(int i=seats.size()-1;i>=0;i--){
-            if(seats[i]==1){
-                int len = seats.size()-1-i;
-                max_gap= max(max_gap,len);
-                break;
-            }
-        }
+        // right edge case
+        int right_gap = seats.size()-1-start;
+        max_gap = max(max_gap,right_gap);
         
         return max_gap;
     }
