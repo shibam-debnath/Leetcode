84c30fa9d968bf144717b70f1b891409aa8b1521
H4saki <shibamdebnath07@gmail.com>
2022-07-06 09:46:17 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.88%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,39 +1,28 @@
-class Solution {
-public:
-    void show_subset(vector<int> &subset,vector<int>& nums,int i,vector<vector<int>> &subsets_all){
+class Solution
+{
+    public:
 
-    //base case
-    if (i >= nums.size())
+        vector<vector < int>> subsets(vector<int> &nums)
+        {
+
+           	//to store each subsets
+            vector<int> subset;
+
+           	//to store all subsets
+            vector<vector < int>> subsets_all;
+
+            show_subset(subset, nums, 0, subsets_all);
+            return subsets_all;
+        }
+
+    void show_subset(vector<int> &subset, vector<int> &nums, int index, vector< vector< int>> &subsets_all)
     {
         subsets_all.push_back(subset);
-        return;
-    }
-        
-    //decision space : here at beginning pos we can choose only current element 
-    //                 not all the element like permutation problem
-        
-
-    //adding the element
-    subset.push_back(nums[i]);
-    show_subset(subset,nums,i+1,subsets_all);
-    subset.pop_back();
-        
-    // not adding the element
-    show_subset(subset,nums,i+1,subsets_all);  
-        
-  }
-    
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        //to store each subsets
-        vector<int> subset;
-        
-        //to store all subsets
-        vector<vector<int>> subsets_all;
-        
-        show_subset(subset,nums,0,subsets_all); 
-
-        return subsets_all;
+        for (int i = index; i < nums.size(); i++)
+        {
+            subset.push_back(nums[i]);
+            show_subset(subset, nums, i + 1, subsets_all);
+            subset.pop_back();
+        }
     }
 };
