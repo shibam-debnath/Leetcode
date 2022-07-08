d7a50ecb5dc4ac59e4a8b5da44df42058ef7d90e
H4saki <shibamdebnath07@gmail.com>
2022-07-08 16:20:33 +0530

    Time: 14 ms (6.62%), Space: 9.6 MB (51.68%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,13 +4,14 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         
-        for(int i=0;i<n;i++){
-            if( target >= matrix[i][0] && target <= matrix[i][m-1]){
-                for(int j=0;j<m;j++){
-                    if(matrix[i][j] == target) return true;
-                }
-            }
+        int row=0,col=m-1;
+        
+        while(row<n && col>=0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
         }
+        
         return false;
     }
 };
