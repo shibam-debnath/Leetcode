661dfc311e7f3da0d63d494627846a97e4f56e43
H4saki <shibamdebnath07@gmail.com>
2022-07-09 22:21:39 +0530

    Time: 488 ms (50.00%), Space: 90.3 MB (50.00%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+public:
+    int latestTimeCatchTheBus(vector<int> &buses, vector<int> &p, int capacity)
+    {
+        unordered_map<int, int> mp;
+        priority_queue<int, vector<int>, greater<int>> bus, pass;
+        int result = 0;
+
+        for (auto b : buses)
+        {
+            bus.push(b);
+        }
+        for (auto arr_time : p)
+        {
+            pass.push(arr_time);
+            mp[arr_time]++;
+        }
+
+        while (!bus.empty())
+        {
+            int c = 0, last_pass = 0;
+            while (c < capacity && pass.top() <= bus.top() && !pass.empty())
+            {
+                last_pass = pass.top();
+                pass.pop(); // store it
+                // also store the arrival time of last passenger since we have to go before him
+                if (mp.find(last_pass - 1) == mp.end())
+                {
+                    result = last_pass - 1;
+                    // cout << result << " ";
+                }
+                c++;
+            }
+
+            // suppose for this bus departure time is 10 and our last_passenger is 3
+            // In this case our ans is 10 because we want the latest time to catch the bus
+            if (last_pass < bus.top() && c < capacity)
+            {
+                result = bus.top();   
+            }
+            bus.pop();
+        }
+        return result;
+    }
+};
