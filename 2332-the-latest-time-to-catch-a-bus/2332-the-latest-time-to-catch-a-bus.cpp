1d779b2c91e8acd4f1656446cf6f00ab8bbd68cd
H4saki <shibamdebnath07@gmail.com>
2022-07-09 22:26:30 +0530

    Time: 561 ms (50.00%), Space: 90.4 MB (50.00%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -4,40 +4,41 @@ public:
     int latestTimeCatchTheBus(vector<int> &buses, vector<int> &p, int capacity)
     {
         unordered_map<int, int> mp;
-        priority_queue<int, vector<int>, greater<int>> bus, pass;
+        priority_queue<int, vector<int>, greater<int>> bus, passenger;
         int result = 0;
 
-        for (auto b : buses)
+        for (auto dep_time : buses)
         {
-            bus.push(b);
+            bus.push(dep_time);
         }
         for (auto arr_time : p)
         {
-            pass.push(arr_time);
+            passenger.push(arr_time);
+            // using un_map so that we can easily search later
             mp[arr_time]++;
         }
 
         while (!bus.empty())
         {
-            int c = 0, last_pass = 0;
-            while (c < capacity && pass.top() <= bus.top() && !pass.empty())
+            int c = 0, last_passenger = 0;
+            while (c < capacity && passenger.top() <= bus.top() && !passenger.empty())
             {
-                last_pass = pass.top();
-                pass.pop(); // store it
-                // also store the arrival time of last passenger since we have to go before him
-                if (mp.find(last_pass - 1) == mp.end())
+                last_passenger = passenger.top(); // store the arrival time of last passenger
+                passenger.pop();
+
+                // find if the arrival time -> last_passenger - 1 is available or not
+                if (mp.find(last_passenger - 1) == mp.end())
                 {
-                    result = last_pass - 1;
-                    // cout << result << " ";
+                    result = last_passenger - 1;
                 }
                 c++;
             }
 
-            // suppose for this bus departure time is 10 and our last_passenger is 3
+            // suppose for this bus departure time is 10 and our last_passenger comes at 3
             // In this case our ans is 10 because we want the latest time to catch the bus
-            if (last_pass < bus.top() && c < capacity)
+            if (last_passenger < bus.top() && c < capacity)
             {
-                result = bus.top();   
+                result = bus.top();
             }
             bus.pop();
         }
