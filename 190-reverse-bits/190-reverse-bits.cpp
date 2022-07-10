91fa1799f0d34f8200851cf417f5a5955f16a9cf
H4saki <shibamdebnath07@gmail.com>
2022-07-10 21:23:39 +0530

    Time: 11 ms (5.20%), Space: 5.9 MB (52.01%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    
+    // n>> 1 -> shifts one position to right 01010 -> 00101
+    // n<< 1 -> shifts one position to left  01010 -> 10100
+    
+    public:
+    uint32_t reverseBits(uint32_t n)
+    {
+        uint32_t ans = 0; 
+        for(int i=0;i<32;i++){
+            
+            ans<<=1;
+            ans = ans | n%2;
+            n>>=1;
+            
+        }
+        return ans;
+    }
+};
