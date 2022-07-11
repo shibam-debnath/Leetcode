685768a2fc6d3262f1f3636dcd4c4f6b13dd929e
H4saki <shibamdebnath07@gmail.com>
2022-07-11 16:39:09 +0530

    Time: 67 ms (40.70%), Space: 21.4 MB (85.36%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int first=0,second=0;
+        
         for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
-                //find missing num , repeated is nums[i]
-                first = nums[i];
-            }
-            mp[nums[i]]++;
+            while(nums[i]!=nums[nums[i]-1]) 
+                swap(nums[i],nums[nums[i]-1]);
         }
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<=nums.size();i++){
-            if(mp.find(i)==mp.end()){
-                second = i;
-                break;
-            }
-        }
-        return {first,second};
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i+1)
+                return {nums[i],i+1};
+            
+        } 
+        return {0,0};
     }
 };
