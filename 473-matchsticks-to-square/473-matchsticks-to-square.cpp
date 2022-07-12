c6bf4860d607d4b6ebd05382ecb2fc3dcfe4beeb
H4saki <shibamdebnath07@gmail.com>
2022-07-12 11:14:02 +0530

    Time: 1133 ms (10.83%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    
+    // Time Complexity :::  O(4^n)
+    // Space Complexity ::: O(n)
+public:
+    #define ll long long
+
+    bool helper(vector<int> &solai, int idx, vector<ll> &sides)
+    {
+        if (idx == solai.size() && sides[0] == 0 && sides[1] == 0 && sides[2] == 0 && sides[3] == 0)
+            return true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (solai[idx] > sides[i])
+                continue; // we don't take this vak
+
+            sides[i] = sides[i] - solai[idx]; // take
+            if (helper(solai, idx + 1, sides))
+                return true;
+            sides[i] = sides[i] + solai[idx]; // backtracking part
+        }
+        return false;
+    }
+
+    bool makesquare(vector<int> &solai)
+    {
+        if (solai.size() < 4)
+            return false;
+        
+        ll sum = accumulate(solai.begin(), solai.end(), 0);
+        
+        if (sum % 4 != 0)
+            return false;
+        
+        ll len = sum / 4;
+
+        vector<ll> sides{len,len,len,len};
+        sort(solai.begin(),solai.end(),greater<int>());
+        
+        return helper(solai, 0, sides);
+    }
+};
