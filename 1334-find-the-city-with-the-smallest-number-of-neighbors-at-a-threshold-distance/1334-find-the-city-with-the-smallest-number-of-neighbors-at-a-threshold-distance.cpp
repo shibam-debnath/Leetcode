e5bede5e52578bacf7986efe8b812d9d146d351c
H4saki <shibamdebnath07@gmail.com>
2022-07-12 17:56:39 +0530

    Time: 104 ms (74.60%), Space: 12.6 MB (49.24%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,14 +1,14 @@
 class Solution
 {
-public:
-    #define INF INT_MAX
-    int findTheCity(int n, vector<vector<int>> &edges, int t)
+    public:
+#define INF INT_MAX
+    int findTheCity(int n, vector<vector < int>> &edges, int t)
     {
-        // use floyd warshall algo
-        vector<vector<int>> dist(n, vector<int>(n, INF));
+       	// use floyd warshall algo
+        vector<vector < long long>> dist(n, vector < long long > (n, INF));
 
-        // Initialize the solution matrix ( v-> no of cities)
-        for (vector<int> e : edges)
+       	// Initialize the solution matrix (v-> no of cities)
+        for (vector<int> e: edges)
         {
             dist[e[0]][e[1]] = e[2];
             dist[e[1]][e[0]] = e[2];
@@ -19,44 +19,39 @@ public:
 
         for (int k = 0; k < n; k++)
         {
-            // Pick all vertices as source one by one
+           	// Pick all vertices as source one by one
             for (int i = 0; i < n; i++)
             {
-                // Pick all vertices as destination for the above picked source
+               	// Pick all vertices as destination for the above picked source
                 for (int j = 0; j < n; j++)
                 {
-                    // If through k we can have a short path from i to j then update that else ignore
-                    //     note such that both aren't inf             i->k dis      k->j dis
-                    if ((dist[i][k] != INF && dist[k][j] != INF) && (dist[i][k] + dist[k][j]) < dist[i][j])
-                    {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+                   	// If through k we can have a short path from i to j then update that else ignore
+                   	//     note such that both aren't inf             i->k dis      k->j dis
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-        int cityCount = INT_MAX;
-        int ans = 0;
+        int mn = INT_MAX, ans = 0;
+
         for (int i = 0; i < n; i++)
         {
-            int len = 0;
-            // Pick all vertices as destination for the above picked source
+            int c = 0;
             for (int j = 0; j < n; j++)
             {
-                cout << dist[i][j] << "  ";
                 if (dist[i][j] <= t)
-                    len++;
-                // cout << dist[i][j] << "  ";
+                {
+                	// reachable
+                    c++;
+                }
             }
-            cout << endl; 
 
-            if (len <= cityCount)
+            if (c <= mn)
             {
+                mn = c;
                 ans = i;
-                cityCount = len;
             }
         }
-
         return ans;
     }
 };
