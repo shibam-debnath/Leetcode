404363a5d3cf867e7b8e38697a4af79268db0cb2
H4saki <shibamdebnath07@gmail.com>
2022-07-12 11:16:57 +0530

    Time: 1228 ms (9.02%), Space: 10 MB (84.07%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -8,17 +8,20 @@ public:
 
     bool helper(vector<int> &solai, int idx, vector<ll> &sides)
     {
-        if (idx == solai.size() && sides[0] == 0 && sides[1] == 0 && sides[2] == 0 && sides[3] == 0)
+        if (idx == solai.size()){
+            for(auto &x : sides){
+                if(x!=0) return false;
+            }
             return true;
+        }
+            
 
         for (int i = 0; i < 4; i++)
         {
-            if (solai[idx] > sides[i])
-                continue; // we don't take this vak
+            if (solai[idx] > sides[i]) continue; // we don't take this vak
 
             sides[i] = sides[i] - solai[idx]; // take
-            if (helper(solai, idx + 1, sides))
-                return true;
+            if (helper(solai, idx + 1, sides)) return true;
             sides[i] = sides[i] + solai[idx]; // backtracking part
         }
         return false;
@@ -26,13 +29,11 @@ public:
 
     bool makesquare(vector<int> &solai)
     {
-        if (solai.size() < 4)
-            return false;
+        if (solai.size() < 4) return false;
         
         ll sum = accumulate(solai.begin(), solai.end(), 0);
         
-        if (sum % 4 != 0)
-            return false;
+        if (sum % 4 != 0) return false;
         
         ll len = sum / 4;
 
