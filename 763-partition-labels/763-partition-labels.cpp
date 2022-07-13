e1c6d99419885c5df132ac7b0168979421931cbe
H4saki <shibamdebnath07@gmail.com>
2022-07-13 12:17:37 +0530

    Time: 5 ms (64.24%), Space: 6.6 MB (88.94%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -6,18 +6,14 @@ class Solution
             int n = s.size();
             
             if(n==1) return {1};
-            // unordered_map<char, int> mp;
             vector<int> ans;
 
             int end = 0, temp_end = 0, start = 0;
 
             while (end < n)
             {
-                if (end == n - 1)
-                {
-                    // ans.push_back(end);
-                    return ans;
-                }
+                if (end == n - 1) return ans;
+
                 end = s.find_last_of(s[start]);
                 for (int i = start + 1; i < end; i++)
                 {
@@ -27,7 +23,6 @@ class Solution
                         if (end == n - 1)
                         {
                             return ans;
-                            // ans.push_back(end);
                         }
                         end = temp_end;
                     }
@@ -36,7 +31,6 @@ class Solution
                 ans.push_back(end-start+1);
                 start = end + 1;
             }
-            // ans.pop_back();
             return ans;
         }
 };
