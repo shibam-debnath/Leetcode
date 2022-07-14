e6cbbec4ad26f3d83c6d15502fb232afd28d5fdf
H4saki <shibamdebnath07@gmail.com>
2022-07-14 19:52:18 +0530

    Time: 20 ms (70.22%), Space: 10.5 MB (88.95%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -4,27 +4,27 @@ class Solution
 
         string rankTeams(vector<string> &votes)
         {
-            
-            int n=votes[0].size();
-            vector<vector<int>> rank(26, vector<int>(n,0));
-            
+
+            int n = votes[0].size();
+            vector<vector < int>> rank(26, vector<int> (n, 0));
+
             for (auto &s: votes)
             {
                 for (int i = 0; i < s.size(); i++)
                 {
-                    rank[s[i]-'A'][i]++;
+                    rank[s[i] - 'A'][i]++;
                 }
             }
 
             string ans = votes[0];
-            
-            sort(ans.begin(),ans.end(),[&](const char &a,const char &b){
-                for(int i=0;i<n;i++){
-                    if(rank[a-'A'][i] != rank[b-'A'][i]) return rank[a-'A'][i] > rank[b-'A'][i];
-                }
-                return a < b;
-            });
 
+            sort(ans.begin(), ans.end(), [& ](const char &a, const char &b)
+            {
+                if (rank[a - 'A'] == rank[b - 'A'])
+                    return a < b;
+                return rank[a - 'A'] > rank[b - 'A'];
+	        });
+            
             return ans;
         }
 };
