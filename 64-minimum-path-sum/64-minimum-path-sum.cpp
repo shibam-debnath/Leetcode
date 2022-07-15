a21cf879fba2f8d5e3f6aabcf9415fe7addd56c0
H4saki <shibamdebnath07@gmail.com>
2022-07-15 11:48:10 +0530

    Time: 19 ms (30.52%), Space: 9.9 MB (68.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
     int dp[201][201];
+    int solve(vector<vector<int>>& grid,int i,int j,int n,int m){
+        // base case 
+        if(i>=n || j>=m) return INT_MAX;
+        if(i==n-1 && j==m-1) return grid[i][j];
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        //logic
+        return dp[i][j] = grid[i][j] + min(solve(grid,i+1,j,n,m),solve(grid,i,j+1,n,m));
+    }
     
     int minPathSum(vector<vector<int>>& grid) {
-        int m= grid.size(),n= grid[0].size();
-  
-        int dp[m][n];
+        int i=0,j=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        memset(dp,-1,sizeof(dp));
         
-        dp[0][0]=grid[0][0];
-        
-        for(int i=1; i<m ; i++) dp[i][0]= dp[i-1][0]+grid[i][0];
-        for(int j=1; j<n ; j++) dp[0][j]= dp[0][j-1]+grid[0][j];
-        
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j]= grid[i][j]+min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[m-1][n-1];
+        return solve(grid,i,j,n,m);
     }
-
-    
 };
