e172449018bf49a8617f10e274425af9d1da180b
H4saki <shibamdebnath07@gmail.com>
2022-07-15 21:52:15 +0530

    Time: 183 ms (26.80%), Space: 49.2 MB (13.78%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,46 +1,32 @@
-class Solution {
-public:
-    // Unbounded knapsack variation
-    // Time Complexity :::  O(mn)
-    // Space Complexity ::: O(mn)
-    
-    int coinChange(vector<int> &coins, int t)
+class Solution
 {
-    int n = coins.size();
-    vector<vector<int>> dp(n + 1, vector<int>(t + 1,INT_MAX-5));
-    
-    // with 0 coins are there we can't form any amount or we need inf coins 
-    for (int i = 0; i <= t; i++)
-    { 
-        dp[0][i] = INT_MAX - 5;
-    }
-        
-    // amount 0 can can be achieved by all coins taking 0 no of coins 
-    for (int i = 0; i <= n; i++)
-    {
-        dp[i][0] = 0;
-    }
-    
-    // Real dp starts from here
-   for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= t; j++)
+    public:
+       	// Unbounded knapsack variation
+
+       	// Time Complexity :::  O(mn)
+       	// Space Complexity ::: O(mn)
+
+        int coinChange(vector<int> &coins, int t)
         {
-            
-            // coin itself is geater than target don't take that ith coin
-            if (coins[i - 1] > j)
+            int n = coins.size();
+            vector<vector < int>> dp(n + 1, vector<int> (t + 1, INT_MAX - 5));
+
+            // for (int i = 0; i <= t; i++) dp[0][i] = INT_MAX - 5; 
+
+            for (int i = 0; i <= n; i++) dp[i][0] = 0;
+
+           	// Real dp starts from here
+            for (int i = 1; i <= n; i++)
             {
-                dp[i][j] = dp[i-1][j];
-            } 
-            
-            // else take min ( nehi lena hein    ||    lena h ) ->if le rhe ho then +1 becoz we are taking one coin and decrement the target val j by that coin i.e coins[i-1]
-            else
-            {
-                dp[i][j] = min(dp[i - 1][j], dp[i][j -(coins[i-1])] + 1);
+                for (int j = 1; j <= t; j++)
+                {
+                    if (coins[i - 1] <= j)
+                    {
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - (coins[i - 1])] + 1);
+                    }
+                    else dp[i][j] = dp[i - 1][j];
+                }
             }
+            return dp[n][t] != INT_MAX - 5 ? dp[n][t] : -1;
         }
-    }
-        
-    return dp[n][t] != INT_MAX-5 ? dp[n][t] : -1;
-}
 };
