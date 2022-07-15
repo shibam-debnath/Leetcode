b852e415f0956f6de9bd07022db2726763afe5e0
H4saki <shibamdebnath07@gmail.com>
2022-07-15 16:01:49 +0530

    Time: 4 ms (86.49%), Space: 8.5 MB (48.17%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -4,20 +4,15 @@ public:
     int lastStoneWeightII(vector<int>& stones) {
         int n=stones.size();
         int sum=0;
-        for(auto &x : stones){
-            sum+=x;
-        }
-        
+        for(auto &x : stones)  sum+=x;
+
         int dp[n+1][sum+1];
-        
         // initalization
         for(int i=0;i<=sum;i++){
-            dp[0][i] = 0;
+            dp[0][i] = 0;             //already 0
         }
         
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
         
         // main dp
         for(int i=1;i<=n;i++){
@@ -27,9 +22,7 @@ public:
                     dp[i][j] = dp[i-1][j-stones[i-1]] || dp[i-1][j]; 
                     // cout << j << " ";
                 }
-                else{
-                    dp[i][j] = dp[i-1][j];    
-                }
+                else dp[i][j] = dp[i-1][j];    
             }
         }
         
