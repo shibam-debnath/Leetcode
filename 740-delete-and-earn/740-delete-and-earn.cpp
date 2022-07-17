6651ae9ac022099a276e6b905eac929b51530166
H4saki <shibamdebnath07@gmail.com>
2022-07-17 17:38:15 +0530

    Time: 25 ms (47.60%), Space: 16.4 MB (15.86%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,20 +3,19 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int n=10001;
         
+        vector<int> v(n);
         vector<int> dp(n);
-        unordered_map<int,int> mp;
         
-        for(auto &x : nums){
-            mp[x]+=x;
+        for(auto i : nums){
+            v[i] += i;
         }
         
         dp[0]=0;
-        dp[1]=mp[1];
+        dp[1]=v[1];
         
-        for(int i=2;i<n;i++){
-            // suppose we are at 3
-            //         3 ka count + 1 ka count / 2 ka count 
-            dp[i]=max((mp[i]+dp[i-2]),dp[i-1]);
+        
+        for (int i=2;i<n;i++){
+            dp[i] = max(dp[i-2]+v[i] , dp[i-1]);
         }
         
         return dp[n-1];
