61560cb1b75bbc623115349b515e483a4d0b02db
H4saki <shibamdebnath07@gmail.com>
2022-07-19 10:58:36 +0530

    Time: 8 ms (74.53%), Space: 6.8 MB (62.25%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        int n=num1.size(),m=num2.size();
-        vector<int> ans(n+m,0);
+        int n=num1.size();
+        int m=num2.size();
+        
+        vector<int> res(n+m,0);
         
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                // first multiply and store the val+previously stored val in the pos
-                ans[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
-                // since we need to store only the last ele here
-                // store num/10(remaining val) in usse pehle pos mein
-                ans[i+j] += ans[i+j+1]/10;
-                // waha bas first digit ko rakho
-                ans[i+j+1] = ans[i+j+1]%10;
+                
+                res[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+                int sum = res[i+j+1];
+                res[i+j] += sum/10;
+                res[i+j+1] = sum%10;
             }
         }
-        num1.clear();
-        int i=0;
-        while(ans[i]==0 && i!=ans.size()-1) i++;
-        while(i<ans.size()){
-            num1+=(ans[i]+'0');
-            i++;
+        
+        // remove leading zeros
+        int k=0;
+        while(res[k]==0 && k!= res.size()-1){
+            k++;
         }
+        
+        num1.clear();
+        while(k<res.size()){
+            num1+=(res[k]+'0');
+            k++;
+        }
+        
         return num1;
     }
 };
