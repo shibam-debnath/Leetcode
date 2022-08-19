9fde3bcabd71800fb3e74fa2ed85f7881d93cef5
H4saki <shibamdebnath07@gmail.com>
2022-08-19 19:59:32 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (89.87%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -6,19 +6,26 @@ public:
         int n = nums.size();
         if (n == 1)
             return 0;
-        // to handle edge cases we are starting from 1 to n-2 since mid+1 & mid-1 may cause errors
+
         int left = 1, right = n - 2;
+        
+        
         while (left <= right)
         {
-            int mid = right + (left - right) / 2;
-            // peek condition
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            int mid = left+(right-left)/2;
+            
+            if(nums[mid] > nums[mid-1] and nums[mid]> nums[mid+1]){
                 return mid;
-            else if (nums[mid + 1] > nums[mid])
-                left = mid + 1; // go right agar right wala bada dikhe to
-            else if (nums[mid - 1] >= nums[mid])
-                right = mid - 1; // go left agar left wala bada dikhe
+            }
+            else if(nums[mid+1]>nums[mid]){
+                left = mid+1;
+            }
+            else if(nums[mid-1] >= nums[mid]){
+                right = mid-1;
+            }
         }
+        
+        
         // handle edge cases
         if (nums[0] > nums[1])
             return 0;
