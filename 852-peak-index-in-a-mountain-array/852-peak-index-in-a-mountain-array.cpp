5153c3c0b0313d13f036c1e39578ed748ce8d234
H4saki <shibamdebnath07@gmail.com>
2022-08-19 19:48:12 +0530

    Time: 183 ms (34.11%), Space: 59.7 MB (12.46%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -3,25 +3,26 @@ class Solution
     public:
        	// can be solved with O(logn) also
        	// find the occurance of first down slope
+    
+    
+        // O(logN) solution
         int peakIndexInMountainArray(vector<int> &nums)
         {
-            int low = 0, high = nums.size() - 1;
             int ans = -1;
-
-            while (low <= high)
-            {
-                int mid = low + (high - low) / 2;
-                if (nums[mid]>nums[mid+1])
-                {
+            int low = 0, high = nums.size()-1;
+            
+            while(low <= high){
+                int mid = low + (high-low)/2;
+                // find first occurance of down slope
+                if(nums[mid]>nums[mid+1]){
                     ans = mid;
-                    high = mid - 1;	// Look for better answer to the left
+                    high = mid-1;
                 }
-                else
-                {
-                    low = mid + 1;
+                else{
+                    low = mid+1;
                 }
             }
-
+            
             return ans;
         }
 };
