17133b6f88efbd2546ab0ece35333e9f21c02cc2
H4saki <shibamdebnath07@gmail.com>
2022-08-26 08:20:04 +0530

    Time: 6 ms (50.57%), Space: 6 MB (68.93%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string given_no = to_string_and_sort(n);
+        for(int i=0;i<32;i++){
+            int pow_2 = (1<<i);
+            string pow_of_2 = to_string_and_sort(pow_2);
+            if(given_no == pow_of_2) return true;
+        }
+        return false;
+    }
+    
+    string to_string_and_sort(int n){
+        string s = to_string(n);
+        sort(s.begin(),s.end());
+        return s;
+    }
+    
+};
