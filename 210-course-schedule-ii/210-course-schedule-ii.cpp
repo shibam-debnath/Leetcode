4feda8d070ffd13c3f2e807e1dcd27a6c7e53c18
H4saki <shibamdebnath07@gmail.com>
2022-08-26 18:37:52 +0530

    Time: 43 ms (31.27%), Space: 13.5 MB (73.51%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+    vector<int> findOrder(int N, vector<vector < int>> &P)
+    {
+
+        vector<vector<int>> G(N); 
+        vector<int> ans, indegree(N);         
+        for(auto& pre : P){
+            G[pre[1]].push_back(pre[0]),       
+            indegree[pre[0]]++;
+        }
+                                 
+        queue<int> q;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            auto t = q.front();q.pop();
+            ans.push_back(t);
+            
+            for(auto ch : G[t]){
+                indegree[ch]--;
+                if(indegree[ch]==0){
+                    q.push(ch);
+                }
+            }
+        }
+        
+       if(ans.size()==N) return ans;
+       return {};
+    }
+};
