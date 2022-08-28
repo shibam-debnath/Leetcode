c8a96dbde5724730ca3df7fc6d42acd83c23e7c4
H4saki <shibamdebnath07@gmail.com>
2022-08-28 19:41:49 +0530

    Time: 154 ms (36.65%), Space: 35.8 MB (17.03%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,37 +1,59 @@
-#define ll long long
-#define pll pair<ll, ll>
-class Solution {
+class Solution
+{
 public:
+#define ll long long
     int MOD = 1e9 + 7;
-    int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pll>> graph(n);
-        for(auto& road: roads) {
-            ll u = road[0], v = road[1], time = road[2];
-            graph[u].push_back({v, time});
-            graph[v].push_back({u, time});
+    int countPaths(int n, vector<vector<int>> &roads)
+    {
+        vector<vector<pair<ll, ll>>> G(n);
+        for (ll i = 0; i < roads.size(); i++)
+        {
+            G[roads[i][0]].push_back({roads[i][1], roads[i][2]});
+            G[roads[i][1]].push_back({roads[i][0], roads[i][2]});
         }
-        return dijkstra(graph, n, 0);
+        return dijkstra(n, G,0);
     }
-    int dijkstra(const vector<vector<pll>>& graph, int n, int src) {
+
+    int dijkstra(int n, vector<vector<pair<ll, ll>>> G,int src)
+    {
         vector<ll> dist(n, LONG_MAX);
-        vector<ll> ways(n);
-        ways[src] = 1;
+        vector<ll> ways(n, 0);
+        priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<>> pq;
+
+        // cost source
+        pq.push({0, src});
         dist[src] = 0;
-        priority_queue<pll, vector<pll>, greater<>> minHeap;
-        minHeap.push({0, 0}); // dist, src
-        while (!minHeap.empty()) {
-            auto[d, u] = minHeap.top(); minHeap.pop();
-            if (d > dist[u]) continue; // Skip if `d` is not updated to latest version!
-            for(auto [v, time] : graph[u]) {
-                if (dist[v] > d + time) {
-                    dist[v] = d + time;
-                    ways[v] = ways[u];
-                    minHeap.push({dist[v], v});
-                } else if (dist[v] == d + time) {
-                    ways[v] = (ways[v] + ways[u]) % MOD;
+        ways[src] = 1; // ways to reach source is 1
+
+        while (!pq.empty())
+        {
+            auto node = pq.top();
+            pq.pop();
+
+            ll curr_dis = node.first;
+            ll curr = node.second;
+            
+            if(curr_dis > dist[curr]) continue;
+
+            for (auto &ch : G[curr])
+            {
+                ll child = ch.first;
+                ll dis = ch.second;
+
+                // update new shortest path if it has smallest path
+                ll new_path = dist[curr] + dis;
+                if (new_path < dist[child])
+                {
+                    dist[child] = new_path;
+                    ways[child] = ways[curr];
+                    pq.push({dist[child], child});
+                }
+                else if (new_path == dist[child])
+                {
+                    ways[child] = (ways[child] + ways[curr]) % MOD;
                 }
             }
         }
-        return ways[n-1];
+        return ways[n - 1];
     }
 };
