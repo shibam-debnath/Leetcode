18d607a99bed67a0b8a649980229cf25a390d177
H4saki <shibamdebnath07@gmail.com>
2022-08-31 15:11:27 +0530

    Time: 4 ms (73.16%), Space: 11 MB (75.19%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,47 +1,37 @@
 class Solution
 {
     public:
-        int bs(vector<int> &nums, int t, int l, int r)
-        {
-
-            while (l <= r)
-            {
-                int m = l + (r - l) / 2;
-
-               	// Check if x is present at mid
-                if (nums[m] == t)
-                    return m;
-
-               	// If x greater, ignore left half
-                if (nums[m] < t)
-                    l = m + 1;
-
-               	// If x is smaller, ignore right half
-                else
-                    r = m - 1;
-            }
-
-            return -1;
-        }
+    
     int search(vector<int> &nums, int target)
     {
-        int n = nums.size();
-        int diff = 1;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] < nums[i - 1])
-            {
-                break;
+        int l =0;
+        int r = nums.size()-1;   
+        
+        while(l<=r){
+            
+            int mid = l+(r-l)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            else if(nums[l]<=nums[mid]){
+                // left side sorted
+                if(target>=nums[l] and target<nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
             }
-            else
-            {
-                diff++;
+            else{
+                // right side unsorted
+                if(target > nums[mid] and target <= nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }                
             }
         }
-        sort(nums.begin(), nums.end());
-        int ans = bs(nums, target, 0, nums.size() - 1);
-        
-        if(ans == -1) return -1;
-        return (ans + diff) % n;
+        return -1;
     }
 };
