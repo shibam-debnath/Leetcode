66d8be1fae2545d5f875161b67309669be29f463
H4saki <shibamdebnath07@gmail.com>
2022-09-01 19:16:40 +0530

    Time: 279 ms (47.41%), Space: 63.3 MB (65.10%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,19 +3,17 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
         if((m*k) > n) return -1;
-           
+            
         int temp_ans = -1;
-        int l = 1;	// since kok can eat min of 1 banana per hr
+        int l = *min_element(bloomDay.begin(), bloomDay.end());
         int r = *max_element(bloomDay.begin(), bloomDay.end());
         
         while(l<=r){
             
             int mid = l+(r-l)/2;
             bool possible = check(mid,bloomDay,m,k);
-            
             if(possible){
                 // it can be a ans but try with smaller speed since we want min val of k
-                // cout << mid << " ";
                 temp_ans = mid;
                 r = mid-1;
             }
@@ -30,18 +28,14 @@ public:
         int n=bloomDay.size();
         int c = 0,res=0;
         for(int i=0;i<n;i++){
-            if(bloomDay[i]<=day){
-                c++;
-            }
-            else{
-                c=0;
-            }
-            
+            if(bloomDay[i]<=day) c++;
+            else c=0;
             if(c==k){
                 res++;
+                if(res==m) return true;
                 c=0;
             }
         }
-        return res>=m;
+        return false;
     }
 };
