28cee9f65c61ea08395d62d5160be822663a4a47
H4saki <shibamdebnath07@gmail.com>
2022-09-01 20:19:29 +0530

    Time: 54 ms (7.33%), Space: 21 MB (5.26%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findBestValue(vector<int>& arr, int t) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        // decide the search space
+        int low = 0;
+        int high = t;
+        int min_diff= INT_MAX;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int diff1 = get_sum(mid,arr,t);
+            int diff2 = get_sum(mid+1,arr,t);
+
+            if(diff1<=diff2){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+    
+    int get_sum(int d,vector<int> arr,int t){
+        int sum = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            sum += min(arr[i], d);
+        }
+        return abs(sum-t);
+    }
+};
