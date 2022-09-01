a760a29489b4fd9abf73f1595274a65653b5d633
H4saki <shibamdebnath07@gmail.com>
2022-09-01 16:38:50 +0530

    Time: 97 ms (25.41%), Space: 19 MB (33.11%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,45 +1,39 @@
 class Solution
 {
+   	// Time Complexity :::  O(n* log2(maxElement))
+   	// Space Complexity ::: O(n)
     public:
-    int KokoTakes(int speed,vector<int> &piles){
-        unsigned long long time_taken=0;
-        for(auto banana : piles){
-            if(banana%speed!=0) time_taken++;
-            time_taken += banana/speed;
-        }
-        return time_taken;
-    }
+        
         int minEatingSpeed(vector<int> &piles, int h)
         {
-            int n = piles.size(),temp_ans = -1;
-
+            int n = piles.size(), temp_ans = -1;
             int l = 1;	// since kok can eat min of 1 banana per hr
             int r = *max_element(piles.begin(), piles.end());
-            int mid = 0;
             
-            if(h==n) return r;
-            
-        	//find occurance of first element from l to h (hour) when our API gives that yes koko can complete 
-            
-            while (l <= r)
-            {
-               	// mid = Koko eats mid bananas per hr
-                mid = l + (r - l) / 2;
-
-                unsigned long long time = KokoTakes(mid,piles);
-
-                // if time required by koko is less than h hour
-                if (time <= h)
-                {
-                    temp_ans = mid;
-                   	//search for better sol
-                    r = mid - 1;
+            while(l<=r){
+                int mid_speed = l+(r-l)/2;
+                long long time = KokoTakes(mid_speed,piles);
+                
+                if(time<=h){
+                    // it can be a ans but try with smaller speed since we want min val of k
+                    temp_ans = mid_speed;
+                    r = mid_speed-1;
                 }
-                else
-                {
-                    l= mid + 1;
+                else{
+                    l=mid_speed+1;
                 }
             }
             return temp_ans;
+        }
+    
+        long long KokoTakes(int speed, vector<int> &piles)
+        {
+            long long time_taken = 0;
+            for (auto banana: piles)
+            {
+                if (banana % speed != 0) time_taken++;
+                time_taken += banana / speed;
+            }
+            return time_taken;
         }
 };
