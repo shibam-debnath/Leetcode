344851d4d17220d4656c2ae6dd7e93753bbdaf97
H4saki <shibamdebnath07@gmail.com>
2022-09-02 12:27:11 +0530

    Time: 63 ms (28.60%), Space: 30.9 MB (20.71%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -13,6 +13,19 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
+        
+        int h1=0, h2=0;
+        TreeNode *l=root,*r=root;
+        while(l){
+            h1++;
+            l=l->left;
+        }
+        while(r){
+            h2++;
+            r=r->right;
+        }
+        
+        if(h1==h2) return pow(2,h1)-1;
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
