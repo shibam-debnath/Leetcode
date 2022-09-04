ddbc1626cccab9970c89c7250859ab308a5bbc5e
H4saki <shibamdebnath07@gmail.com>
2022-09-04 10:56:58 +0530

    Time: 4 ms (83.68%), Space: 12.1 MB (84.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,55 +1,48 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        vector<vector < int>> verticalTraversal(TreeNode *root)
-        {
-            vector<vector < int>> res;
-            map<int, vector < int>> mp;
-            queue<pair<TreeNode*, int>> q;
-            q.push({ root,
-                0 });
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,vector<int>> mp;
 
-            int dis = 0;
-            while (!q.empty())
-            {
-                multiset<pair<int,int>> mmp;
-                int size = q.size();
-                while (size--)
-                {
-                    TreeNode *node = q.front().first;
-                    dis = q.front().second;
-                    q.pop();
-                    
-                    mmp.insert({dis,node->val});
-
-                    if (node->left) q.emplace(node->left, dis - 1);
-                    if (node->right) q.emplace(node->right, dis + 1);
-                }
+        queue<pair<TreeNode *,int>> q;
+        q.push({root,0});
+        
+        while(!q.empty()){
+            int size = q.size(); 
+            map<int,multiset<int>> mp2;
+            while(size--){
+                TreeNode *curr = q.front().first;
+                int level = q.front().second;
+                q.pop();
                 
-                for(auto x : mmp){
-                    mp[x.first].push_back(x.second);
+                mp2[level].insert(curr->val);
+                
+                if(curr->left) q.push({curr->left,level-1});
+                if(curr->right) q.push({curr->right,level+1});
+            }
+            
+            for(auto &m : mp2){
+                int level = m.first;
+                for(auto &x : m.second){
+                    mp[level].push_back(x);
                 }
             }
-
-            vector<int> v;
-            for (auto m: mp)
-            {
-                v.clear();
-                v = m.second;
-                res.push_back(v);
-            }
-
-            return res;
         }
+        
+        vector<vector<int>> res;
+        for(auto &m : mp){
+            res.push_back(m.second);
+        }
+        return res;
+    }
 };
